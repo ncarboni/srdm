793fd0431956db2b62fab40b100942c6323fbe73
Nicola <nicola@carboni.me>
2020-12-05 08:19:23 +0100

    Update artwork.sh

--- docs/et/artworkCSV/artwork.sh
+++ docs/et/artworkCSV/artwork.sh
@@ -14,17 +14,17 @@ rm rm  *.csv ;
 
 unzip -q script.zip
 rm -rf __MACOSX
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shrPgJopfdzS8TRjU > Artwork_Name.csv ;
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shr1BUAc0k7CMGOPR > Artwork_Parthood.csv ;
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shr6LEceTYqarh60o > Artwork_Existence.csv ;
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shrirsDGYFACJmba9 > Artwork_substance.csv ;
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shrhZ6t2B0cTVteq1 > Artwork_subject.csv ;
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shrwlwLhFyzlvHgE6 > Artwork_Events.csv ;
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shriQBo2TMU58eSie > Artwork_Actor_Relations.csv ;
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shrhRUS9bl0QqxRdk > Artwork_Rights.csv ;
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shrnv4u11sMVs3T8M > Artwork_Location.csv ;
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shr6WRc5RLgfooJfM > Artwork_Description.csv ;
-python ../airscraper/airscraper/airscraper/airscraper.pyhttps://airtable.com/shr0LgomaG22z3frW > Artwork_Documentation.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shrPgJopfdzS8TRjU > Artwork_Name.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shr1BUAc0k7CMGOPR > Artwork_Parthood.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shr6LEceTYqarh60o > Artwork_Existence.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shrirsDGYFACJmba9 > Artwork_substance.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shrhZ6t2B0cTVteq1 > Artwork_subject.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shrwlwLhFyzlvHgE6 > Artwork_Events.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shriQBo2TMU58eSie > Artwork_Actor_Relations.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shrhRUS9bl0QqxRdk > Artwork_Rights.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shrnv4u11sMVs3T8M > Artwork_Location.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shr6WRc5RLgfooJfM > Artwork_Description.csv ;
+python ../airscraper/airscraper/airscraper/airscraper.py https://airtable.com/shr0LgomaG22z3frW > Artwork_Documentation.csv ;
 echo "finished download"
 ./csvcut.sh
 cd ttl/
